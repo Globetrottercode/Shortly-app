e5bcf32e7264d91f881af72c8c100e8541aaea87
Globetrottercode <dassouvik180@gmail.com>
2023-02-18 13:44:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Navbar } from "./Navbar.js";
+import { InputArea } from "./InputArea";
+import { UpperSection } from "./UpperSection";
+import { Footer } from "./Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <UpperSection />
+      <InputArea />
+      <Footer />
     </div>
   );
 }
-
-export default App;
